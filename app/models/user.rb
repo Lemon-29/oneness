551184fc2db0de08ec312550b88a9fa052695e01
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-14 02:04:10 +0900

    set css for devise

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :posts, dependent: :destroy
+  # mount_uploader :image, ImageUploader
+
+end
