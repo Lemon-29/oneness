e0457de8ebf46aa84a1452ec7f2cf705b6e1c8b7
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-14 01:42:44 +0900

    set migration:user,post,devise

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @posts  = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :image, :image_cache, :user_id)
+  end
+end
