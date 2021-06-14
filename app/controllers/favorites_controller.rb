05b54578332e1543a5378ba7ac4dc55adcab2296
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-14 23:40:24 +0900

    set controller for favorites

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,13 @@
+class FavoritesController < ApplicationController
+  before_action :authenticate_user! 
+  def create
+    favorite = current_user.favorites.create(post_id: params[:post_id])
+    redirect_to posts_path, notice: 'お気に入りしました'
+  end
+
+  def destroy
+    favorite = current_user.favorites.find_by(id: params[:id])
+    favorite.destroy
+    redirect_to posts_path, notice: 'お気に入りを解除しました'
+  end
+end
