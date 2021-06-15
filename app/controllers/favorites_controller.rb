dd1a0ab77971987489eabdbe88a67f76bc549628
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-15 22:36:59 +0900

    add function:devise of name

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,14 @@ class FavoritesController < ApplicationController
   before_action :authenticate_user! 
   def create
     favorite = current_user.favorites.create(post_id: params[:post_id])
-    redirect_to post_path("#{params[:post_id]}"),notice: 'お気に入りに追加しました'
+    redirect_to post_path(params[:post_id]),notice: 'お気に入りに追加しました！'
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id])
     favorite.destroy
     # @favorite = Favorite.find_by(user_id: current_user.id, post_id: params[:post_id])
-    redirect_to post_path("#{params[:post_id]}"),notice: 'お気に入りを解除しました'
+    redirect_to posts_path,notice: 'お気に入りを解除しました！'
   end
 
   def show
