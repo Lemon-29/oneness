31e43cf51da7feaa47fce81776aea50e22a8660b
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-17 21:00:37 +0900

    change profile piture's size

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def guest_sign_in
-    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+    user = User.find_or_create_by!(email: 'guest@example.com', name: 'Guest') do |user|
       user.password = SecureRandom.urlsafe_base64
   end
     sign_in user
