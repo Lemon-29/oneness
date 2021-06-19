d6c31011def0312419bdffcdc93efcfe0e11c7c1
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-19 18:23:23 +0900

    set header sidebar layouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
       user.password = SecureRandom.urlsafe_base64
   end
     sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+    redirect_to posts_path, notice: 'ゲストユーザーとしてログインしました。'
   end
 
   def index
