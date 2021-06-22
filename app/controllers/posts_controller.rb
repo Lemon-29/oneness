e33dbd8696eb77f1493af3e100509f00cffdb87c
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-22 17:34:42 +0900

    fix alert style and post access redirect

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :create]
+  before_action :authenticate_user!
   before_action :set_post, only: [:edit, :update, :show]
 
   def index
