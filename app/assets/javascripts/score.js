8ba6a52d0169059421311612313814a38eee6c13
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-22 16:40:17 +0900

    set score of rate in comment index

--- app/assets/javascripts/score.js
+++ app/assets/javascripts/score.js
@@ -14,7 +14,19 @@ function showHalfScores({
   });
 }
 
-function showReadOnlyHalfScores() {
+function showReadonlyHalfScores({
+  starOff,
+  starOn,
+  starHalf,
+}) {
   const elements = $('.app-score_readonly');
   if (elements.length == 0) return;
+  elements.raty({
+    starOff,
+    starOn,
+    starHalf,
+    readOnly: true,
+    half:  true,
+    hints: [['bad 1/2', 'bad'], ['poor 1/2', 'poor'], ['regular 1/2', 'regular'], ['good 1/2', 'good'], ['gorgeous 1/2', 'gorgeous']]
+  });
 }
