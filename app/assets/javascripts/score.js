b603124c790ae3b67563e11e0b45ae746df7ef85
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-23 01:12:31 +0900

    set comment raty index with ajax

--- app/assets/javascripts/score.js
+++ app/assets/javascripts/score.js
@@ -38,3 +38,45 @@ function showReadonlyHalfScores({
     hints: [['bad 1/2', 'bad'], ['poor 1/2', 'poor'], ['regular 1/2', 'regular'], ['good 1/2', 'good'], ['gorgeous 1/2', 'gorgeous']]
   });
 }
+
+
+function showHalfScoresWithSelector(selector, where, {
+  starOff,
+  starOn,
+  starHalf,
+  scoreName,
+  size,
+}) {
+  const elements = $(selector).find(where);
+  if (elements.length == 0) return;
+  elements.raty({
+    starOff,
+    starOn,
+    starHalf,
+    half:  true,
+    hints: [['bad 1/2', 'bad'], ['poor 1/2', 'poor'], ['regular 1/2', 'regular'], ['good 1/2', 'good'], ['gorgeous 1/2', 'gorgeous']],
+    size,
+    scoreName,
+  });
+}
+
+function showReadonlyHalfScoresWithSelector(selector, where, {
+  starOff,
+  starOn,
+  starHalf,
+  scoreName,
+  size,
+}) {
+  const elements = $(selector).find(where);
+  if (elements.length == 0) return;
+  elements.raty({
+    starOff,
+    starOn,
+    starHalf,
+    readOnly: true,
+    half:  true,
+    hints: [['bad 1/2', 'bad'], ['poor 1/2', 'poor'], ['regular 1/2', 'regular'], ['good 1/2', 'good'], ['gorgeous 1/2', 'gorgeous']],
+    size,
+    scoreName,
+  });
+}
