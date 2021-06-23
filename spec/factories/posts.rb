beceb3d219809b17ca881484c699f34e5add3835
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-23 10:58:44 +0900

    set post factorybot

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,12 @@
 FactoryBot.define do
   factory :post do
     content { "Post1" }
+    image { File.open("./app/assets/images/pose1-1.jpg"), }
+    association :user, factory: :user
+  end
+  factory :post2 do
+    content { "Post2" }
     image { File.open("./app/assets/images/pose1-2.jpg"), }
+    association :user, factory: :admin_user
   end
 end
