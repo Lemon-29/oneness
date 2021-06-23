39144759070faec3adbb0ce9fdb9d7eaeaa3caf3
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-23 11:01:35 +0900

    re

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,26 @@
 FactoryBot.define do
   factory :comment do
-    comment_content { "MyString" }
-    user { nil }
-    post { nil }
+    content { "MyString" }
+    score { 3.5 }
+    association :post, factory: :post
+    association :user, factory: :user
+  end
+  factory :comment2 do
+    content { "YEAR" }
+    score { 1.0 }
+    association :post, factory: :post
+    association :user, factory: :admin_user
+  end
+  factory :comment do
+    content { "MyString2" }
+    score { 3.0 }
+    association :post, factory: :post2
+    association :user, factory: :user
+  end
+  factory :comment2 do
+    content { "YEAR2" }
+    score { 4.5 }
+    association :post, factory: :post2
+    association :user, factory: :admin_user
   end
 end
