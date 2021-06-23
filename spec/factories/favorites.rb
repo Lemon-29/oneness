a85d8501af6255d50faa2c93e338248749240a1a
Lisa Takahashi <paris.tokyo.77777@gmail.com>
2021-06-23 10:59:54 +0900

    set favorite factory

--- spec/factories/favorites.rb
+++ spec/factories/favorites.rb
@@ -1,7 +1,25 @@
 FactoryBot.define do
   factory :favorite do
-    post_id { 1 }
-    user_id { 1 }
+    association :post, factory: :post
+    association :user, factory: :user
+    created_at { "2021-06-14 22:28:32" }
+    updated_at { "2021-06-14 22:28:32" }
+  end
+  factory :favorite2 do
+    association :post2, factory: :post2
+    association :user, factory: :user
+    created_at { "2021-06-14 22:28:32" }
+    updated_at { "2021-06-14 22:28:32" }
+  end
+  factory :favorite3 do
+    association :post, factory: :post
+    association :user, factory: :admin_user
+    created_at { "2021-06-14 22:28:32" }
+    updated_at { "2021-06-14 22:28:32" }
+  end
+  factory :favorite4 do
+    association :post2, factory: :post2
+    association :user, factory: :admin_user
     created_at { "2021-06-14 22:28:32" }
     updated_at { "2021-06-14 22:28:32" }
   end
